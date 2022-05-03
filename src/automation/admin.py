5eaecef6d2068f39f5961067241784535654f18b
Nana Duah <gabano2005@gmail.com>
2022-05-02 20:33:13 -0400

    Add migration

--- src/automation/admin.py
+++ src/automation/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 
 from .models import Job
-from .models import Message
-
-admin.site.register(Message)
-
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
