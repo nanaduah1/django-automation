2c217c5355ef266b69f4266417204f35e2f891bf
Nana Duah <gabano2005@gmail.com>
2022-05-07 12:49:30 -0400

    Added worker test base

--- src/automation/tests.py
+++ src/automation/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
 
-# Create your tests here.
+from automation.models import Job
+
+
+class WorkerTestMixin:
+    worker_class = None
+
+    def setUp(self) -> None:
+        super().setUp()
+        assert self.worker_class is not None
+        self.worker_class.start()
+        self.job = Job.objects.get(type_key=self.worker_class.key)
+        self.worker = self.worker_class(self.job)
+
+    def test_worker_runs_successfully(self):
+        self.worker.execute()
+        self.assertEqual(self.job.status, Job.STATUS_COMPLETED)
+
+
